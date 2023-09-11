39f4255fbf985be8ad2b9087bf694f41dac37e8e
Lei <ylxx@live.com>
2023-09-11 21:38:26 +0800

    u

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 
 # Reference
 - [Probability Playground - Buffalo](https://www.acsu.buffalo.edu/~adamcunn/probability/normal.html)
+- [DDPM - dataflowr](https://github.com/dataflowr/notebooks/tree/master/Module18)
--- distribution_playground/source_distribution.py
+++ distribution_playground/source_distribution.py
@@ -3,9 +3,13 @@ import boxx
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+from scipy.stats import entropy
+
 eps = 1e-20
 
-def draw_circles_and_blur(s):
+
+def draw_circles_and_blur(size=100):
+    s = size
     # 创建一个 s x s 的空白图像
     img = np.zeros((s, s), dtype=np.uint8)
 
@@ -15,63 +19,65 @@ def draw_circles_and_blur(s):
         (s // 4, s // 4),
         (s * 3 // 4, s // 4),
         (s // 4, s * 3 // 4),
-        (s * 3 // 4, s * 3 // 4)
+        (s * 3 // 4, s * 3 // 4),
     ]
 
     # 画 5 个圆
     radius = s // 10
     for center in circles_centers:
         cv2.circle(img, center, radius, color=128, thickness=-1)
-        cv2.circle(img, center, radius//2, color=0, thickness=-1)
+        cv2.circle(img, center, radius // 2, color=0, thickness=-1)
 
     # 使用高斯模糊核半径为 s//50
     ksize = (s // 40) * 2 + 1
     blurred_img1 = cv2.GaussianBlur(img, (ksize, ksize), 0)
-    
+
     ksize = (s // 10) * 2 + 1
     blurred_img2 = cv2.GaussianBlur(img, (ksize, ksize), 0)
 
     ksize = int((s / 2.5) * 2 + 1)
     blurred_img3 = cv2.GaussianBlur(img, (ksize, ksize), 0)
-    s = s//2
-    img[:s,s:] = blurred_img1[:s,s:]
-    img[s:,:s] = blurred_img2[s:,:s]
-    img[s:,s:] = blurred_img3[s:,s:]
-    img = img + eps/img.size
-    return img/img.sum()
+    s = s // 2
+    img[:s, s:] = blurred_img1[:s, s:]
+    img[s:, :s] = blurred_img2[s:, :s]
+    img[s:, s:] = blurred_img3[s:, s:]
+    img = img + eps / img.size
+    return img / img.sum()
 
-def sample_probability_density(arr, K=1000, domain=None):
-    n = arr.ndim
-    domain = np.array([(-1,1)]*n) if domain is None else domain
-    assert domain.shape == (n, 2)
+
+def sample_probability_density(arr, n=1000, domain=None):
+    s = arr.ndim
+    domain = np.array([(-1, 1)] * s) if domain is None else domain
+    assert domain.shape == (s, 2)
 
     # 计算累积分布函数
     cdf = np.cumsum(arr)
-    
-    # 使用均匀随机样本，在[0, 1]范围内生成K个点
-    random_samples = np.random.rand(K)
-    
+
+    # 使用均匀随机样本，在[0, 1]范围内生成n个点
+    random_samples = np.random.rand(n)
+
     # 计算每个随机样本对应的索引值
     indices = np.searchsorted(cdf, random_samples)
     indices = np.unravel_index(indices, arr.shape)
-    
+
     # 将索引值映射到定义域中的相应位置
-    samples = np.empty((K, n))
-    for i in range(n):
+    samples = np.empty((n, s))
+    for i in range(s):
         left, right = domain[i]
         size = arr.shape[i]
-        
+
         # 计算每个维度上的步长
         step = (right - left) / size
-        
+
         # 将索引值映射到定义域
-        idxs = indices[i] 
+        idxs = indices[i]
         if "uniform in one pixel":
-            idxs = idxs + np.random.uniform(-.5+eps, .5-eps, idxs.shape)
-        samples[:, i] = left + idxs* step + step / 2
-    
-    xys = samples[:, ::-1]*[[1,-1]]
+            idxs = idxs + np.random.uniform(-0.5 + eps, 0.5 - eps, idxs.shape)
+        samples[:, i] = left + idxs * step + step / 2
+
+    xys = samples[:, ::-1] * [[1, -1]]
     return xys
+
 
 def samples_to_indices(samples, domain, density):
     arr_shape = density.shape
@@ -79,54 +85,107 @@ def samples_to_indices(samples, domain, density):
     assert domain.shape == (n, 2)
 
     indices = np.empty((len(arr_shape), len(samples)), dtype=np.int32)
-    
+
     for i in range(n):
         left, right = domain[i]
         size = arr_shape[i]
         # 计算每个维度上的步长
         step = (right - left) / size
         # 计算索引值
-        indices[i] = np.round((samples[:, i].clip(left+eps, right-eps) - left - step / 2.) / step).astype(int)
+        indices[i] = (
+            np.round((samples[:, i] - left - step / 2.0) / step)
+            .astype(int)
+            .clip(0, size - 1)
+        )
     return indices.T
 
-class DistributionByDensityArray():
+
+def jensen_shannon_divergence(p, q):
+    """
+    Compute the Jensen-Shannon divergence between two probability distributions.
+    :param p: First probability distribution
+    :param q: Second probability distribution
+    :return: JS divergence value
+    """
+    p = np.asarray(p, dtype=np.float)
+    q = np.asarray(q, dtype=np.float)
+
+    m = 0.5 * (p + q)
+    jsd = 0.5 * (entropy(p, m) + entropy(q, m))
+    return jsd
+
+
+class DistributionByDensityArray:
     def __init__(self, density, domain=None):
         self.density = density
         n = density.ndim
-        self.domain = np.array([(-1,1)]*n) if domain is None else domain
-    def sample(self, K=1000):
-        return sample_probability_density(self.density, K, self.domain)
-    
+        self.domain = np.array([(-1, 1)] * n) if domain is None else domain
+
+    def sample(self, n=1000):
+        return sample_probability_density(self.density, n, self.domain)
+
     def __str__(self):
         return f"{self.__class__.__name__}(density={self.density.shape}, domain={list(map(tuple,self.domain))})"
-    
+
     def kl_divergence(self, xys):
-        samples = (xys*[[1,-1]])[:, ::-1]
+        samples = (xys * [[1, -1]])[:, ::-1]
         sample_idxs = samples_to_indices(samples, self.domain, self.density)
         unique, count = np.unique(sample_idxs, axis=0, return_counts=True)
-        original = self.density.flatten()
+        Q = self.density.flatten()
         estimated_count = np.zeros_like(self.density)
-        estimated_count[unique[:, 0],unique[:, 1],] = count 
-        estimated = estimated_count/estimated_count.sum()
-        estimated = estimated/estimated.sum()
-        from scipy.stats import entropy
-        kl_loss = entropy(estimated.flatten(), original)
+        estimated_count[
+            unique[:, 0],
+            unique[:, 1],
+        ] = count
+        estimated = estimated_count / estimated_count.sum()
+        estimated = estimated / estimated.sum()
+        P = estimated.flatten()
+        # gt 分布是预构建的可以直接加 eps 而不影响精度, 所以 gt 分布作为分母
+        kl_loss = entropy(P, Q)
+        js_loss = jensen_shannon_divergence(P, Q)
+        tv_loss = 0.5 * np.abs(P - Q).sum()
+        hellinger = np.sqrt(0.5 * np.sum((np.sqrt(P) - np.sqrt(Q)) ** 2))
         boxx.mg()
-        return dict(kl=kl_loss,estimated=estimated)
-    
-    __repr__ = __str__
-if __name__ == "__main__" :
-    from boxx import *
-    
-    # 设置边长s
-    s = 100
-    density = draw_circles_and_blur(s)
-    xys = sample_probability_density(density, 10000)
-    dist = DistributionByDensityArray(density, )
-    xys = dist.sample(1000000)
-    kl = dist.kl_divergence(xys)
-    print(kl['kl'])
-    show(kl, density, histEqualize)
-    # plt.scatter(xys[:, 0], xys[:, 1], alpha=0.15)
-    
+        return dict(
+            kl=kl_loss,
+            js=js_loss / np.log(2),
+            js_raw=js_loss,
+            tv=tv_loss,
+            hellinger=hellinger,
+            estimated=estimated,
+        )
 
+    divergence = kl_divergence
+
+    def str_divergence(self, divergence):
+        if not isinstance(divergence, dict):
+            divergence = self.divergence(divergence)
+        return ", ".join(
+            [f"{k}={divergence[k]}" for k in ["kl", "js", "tv", "hellinger"]]
+        )
+
+    __repr__ = __str__
+
+
+def get_test_dist(size=100):
+    density = draw_circles_and_blur(size)
+    dist = DistributionByDensityArray(
+        density,
+    )
+    return dist
+
+
+if __name__ == "__main__":
+    from boxx import *
+
+    size = 100
+    density = draw_circles_and_blur(size)
+    dist = DistributionByDensityArray(
+        density,
+    )
+    xys = dist.sample(10000)
+    divergence = dist.kl_divergence(xys)
+    [print(k, "=", divergence[k]) for k in ["kl", "js", "tv", "hellinger"]]
+    show(divergence, density, histEqualize)
+    plt.scatter(xys[:, 0], xys[:, 1], alpha=0.15)
+    plt.axis("equal")
